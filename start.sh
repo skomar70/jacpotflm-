854b91671077a9063c44f6cb7618b9de5183e83d
skomar70 <mosharofroydev@gmail.com>
2025-09-21 18:37:42 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 # Start script for jacpotflm- Bot
 
-# Clone repo
-if [ -z "$UPSTREAM_REPO" ]; then
-    echo "Cloning main Repository"
-    git clone https://github.com/skomar70/jacpotflm- /jacpotflm-
-else
-    echo "Cloning Custom Repo from $UPSTREAM_REPO"
-    git clone "$UPSTREAM_REPO" /jacpotflm-
-fi
-
 # Go to project directory
 cd /jacpotflm- || exit
-
-# Install dependencies
-pip3 install --no-cache-dir -U -r requirements.txt
 
 # Start the bot
 echo "Bot Started...."
