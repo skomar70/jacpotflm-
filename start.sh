d0df943df45e812f123e416718cb30cbaa23b6be
skomar70 <mosharofroydev@gmail.com>
2025-09-23 17:14:15 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
-# Start bot from current WORKDIR
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/skomar70/AutoFilterbot /AutoFilterbot
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /AutoFilterAdvance
+fi
+cd /AutoFilterbot
+pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
