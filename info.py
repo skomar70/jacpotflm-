a82956c5ad5e4054ba67b2505f663ac0156e7484
skomar70 <mosharofroydev@gmail.com>
2025-09-25 05:25:40 +0000

    Update info.py

--- info.py
+++ info.py
@@ -1,29 +1,108 @@
-# Telegram Bot Config
-BOT_TOKEN = "8328198762:AAE2X2atmTOdSK_A1C0izn44q2b47FNqrxs"
-API_ID = 24776633
-API_HASH = "57b1f632044b4e718f5dce004a988d69"
+ import re
+from os import environ
 
-# Admin User
-ADMINS = [8248792819]  # Telegram ID
-LANG = "en"
-FIRST_NAME = "sk"
-LAST_NAME = "Omar"
+# =========================
+# Helper Functions
+# =========================
+id_pattern = re.compile(r'^.\d+$')
 
-# Channels
-AUTH_CHANNEL = -1002926169752      # aouth channel
-FILE_STORE_CHANNELS = [-1003036018855, -1003086003339]  # file channel 1 & 2
-LOG_CHANNEL = -1002996983894       # log channel
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
 
-# Branding / Links (new group link)
-BRANDING_TEXT = "<a href='https://t.me/jacpotfilmm'>Jacpotfilm</a>"
-VERIFY_LINK = "https://yourdomain.com/direct_video.mp4"
-TUTORIAL_LINK = "https://t.me/jacpotfilmm"
+# =========================
+# Bot Information
+# =========================
+SESSION = environ.get('SESSION', 'Media_search')
+API_ID = int(environ.get('API_ID', '24776633'))
+API_HASH = environ.get('API_HASH', '57b1f632044b4e718f5dce004a988d69')
+BOT_TOKEN = environ.get('BOT_TOKEN', "8328198762:AAE2X2atmTOdSK_A1C0izn44q2b47FNqrxs")
+PORT = int(environ.get('PORT', '8080'))
 
+# =========================
 # Bot Settings
-ADS_ENABLED = True
-TUTORIAL_ENABLED = True
-VIDEO_EXPIRY_DAYS = 5
-AUTO_DELETE_TIME = 300  # seconds
+# =========================
+CACHE_TIME = int(environ.get('CACHE_TIME', 300))
+USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
+PICS = (environ.get('PICS', 'https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg')).split()
 
-# MongoDB
-MONGO_URI_MAIN = "mongodb+srv://ryamoni:YQs5Qy4CdpGpo8zS@cluster0.actpio3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+# =========================
+# Admins, Channels & Users
+# =========================
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '8248792819').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1003086003339').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+AUTH_USERS = (auth_users + ADMINS) if auth_users else []
+auth_grp = environ.get('AUTH_GROUP')
+AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
+
+# =========================
+# MongoDB Information
+# =========================
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://test:hvM1kLeXCCSXXb1u@cluster0.mrfgvxy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+
+# =========================
+# FSUB / Channels
+# =========================
+auth_channel = environ.get('AUTH_CHANNEL', '')
+AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
+
+REQ_CHANNEL = environ.get("REQ_CHANNEL", '-1002926169752')
+REQ_CHANNEL = int(REQ_CHANNEL) if REQ_CHANNEL and id_pattern.search(REQ_CHANNEL) else False
+JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
+
+# =========================
+# Others
+# =========================
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002965828075'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'VJ_Bot_Disscussion')
+P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), False)
+IMDB = is_enabled((environ.get('IMDB', "False")), True)
+SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), False)
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", """<b>ɴᴀᴍᴇ: <code>{file_name}</code> \n\nJᴏɪɴ Nᴏᴡ: [⚡Join Update Channel⚡](https://t.me/searchhelptm)</b>""")
+BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>Query: {query}</b> \n‌‌‌‌IMDb Data:\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10")
+LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
+SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "False"), True)
+MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
+INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1001722984461')).split()]
+MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "False")), True)
+PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
+PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+
+# =========================
+# Token / Shortlink Verification Settings
+# =========================
+VERIFY = bool(environ.get('VERIFY', True))
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'https://short.link/example')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'https://api.shortlink.com/verify')
+VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://youtube.com/tutorial')
+
+VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
+VERIFY_SND_SHORTLINK_URL = environ.get('VERIFY_SND_SHORTLINK_URL', '')
+VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
+
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://short.link/earn')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'https://api.shortlink.com/check')
+TUTORIAL = environ.get('TUTORIAL', 'https://youtube.com/tutorial')
+
+# =========================
+# Log Summary
+# =========================
+LOG_STR = "Current Cusomized Configurations are:-\n"
+LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
+LOG_STR += ("P_TTI_SHOW_OFF found , Users will be redirected to send /start to Bot PM instead of sending file file directly\n" if P_TTI_SHOW_OFF else "P_TTI_SHOW_OFF is disabled files will be send in PM, instead of sending start.\n")
+LOG_STR += ("SINGLE_BUTTON is Found, filename and files size will be shown in a single button instead of two separate buttons\n" if SINGLE_BUTTON else "SINGLE_BUTTON is disabled , filename and file_sixe will be shown as different buttons\n")
+LOG_STR += (f"CUSTOM_FILE_CAPTION enabled with value {CUSTOM_FILE_CAPTION}, your files will be send along with this customized caption.\n" if CUSTOM_FILE_CAPTION else "No CUSTOM_FILE_CAPTION Found, Default captions of file will be used.\n")
+LOG_STR += ("Long IMDB storyline enabled." if LONG_IMDB_DESCRIPTION else "LONG_IMDB_DESCRIPTION is disabled , Plot will be shorter.\n")
+LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies if movie not found\n" if SPELL_CHECK_REPLY else "SPELL_CHECK_REPLY Mode disabled\n")
+LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
+LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
